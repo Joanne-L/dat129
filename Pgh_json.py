df2493008395c0783336db4e9d5737f5e87916d3
JoanneLauer <laue04@acd.ccac.edu>
2020-09-23 20:18:53 -0400

    Initial Commit

---
+++ Pgh_json.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+## Joanne
+## Json Project
+## 9-23-2020
+
+import json
+
+with open('path_to_file/person.json') as f:
+  data = json.load(f)
+
+json_string = '{"key1": "value1", "key2": "value2", "key3": "value3", "key4": "value4", "key5": "value5", "key6": "value6","key7": "value7","key8": "value8","key9": "value9","key10": "value10"}'
+json_dictionary = json.loads(json_string)
+for key in json_dictionary:
+    print(key, ":", json_dictionary[key])
+    
+# method logMalformedProject loop is called when project does not contain area and writes the project if to a file
+
+def logMalformedProject()
+    for entry in json_dictionary
+    if value5 
+    else
+    
+# method assembles a list of unique values for the project area 
+
+def log_Area()
+    for entry in json_dictionary
+    if value5 
+    else
+    
+# method assembles a list of unique values for 'status'
+
+def log_status()
+    for entry in json_dictionary
+    if value10 
+    else
+    
+# method assembles a list of unisue values for 'asset_type' 
+
+def log_asset_type()
+    for entry in json_dictionary
+    if value4 
+    else
+    
+ # method assembles a list of unisue values for 'fiscal_year'  
+
+    def log_fiscal_yeay()
+    for entry in json_dictionary
+    if value8 
+    else
+print(data)
+
+def main()
+main()
+
+#json.dumps
+#print (json.dumps)
+#json_string = json.dumps(data)
+#json.dumps(data)
+#json.dumps(data, indent=4)
+
+
+
+
+# In[ ]:
+
+
+
+
